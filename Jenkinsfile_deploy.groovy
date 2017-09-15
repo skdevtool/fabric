2d0da8ec3da1f18d0b47bf3c7cc3d6801033c179
skdevtool <devtool@outlook.kr>
2017-09-15 11:51:29 +0900

    test

--- Jenkinsfile_deploy.groovy
+++ Jenkinsfile_deploy.groovy
@@ -8,11 +8,7 @@ pipeline {
 		stage('Deploy') {
 			steps {
             	echo "docker login (remote)"
-				sh "echo ${DOCKER_SECRET}"
-				echo DOCKER_SECRET
-				echo DOCKER_SECRET_USR
-
-			    sh "docker -H tcp://10.178.84.172:2375 login -u admin -p admin123 nexus.sk.com"
+			    sh "docker -H tcp://10.178.84.172:2375 login -u ${DOCKER_SECRET_USR} -p ${DOCKER_SECRET_PWD} nexus.sk.com"
 			    echo "docker stack deploy (remote)"
                 sh "docker -H tcp://10.178.84.172:2375 stack deploy -c docker-stack.yaml --resolve-image changed --with-registry-auth fabric-net"
 			}
