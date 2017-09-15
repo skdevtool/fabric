15682ee7f23c6dcf90b4fb0dde39cb8650740ce1
skdevtool <devtool@outlook.kr>
2017-09-15 12:36:08 +0900

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -31,7 +31,7 @@ pipeline {
                 echo "docker login (remote)"
 			    sh "docker -H tcp://${DOCKER_REMOTE_HOST} login -u ${REPO_TOKEN_USR} -p ${REPO_TOKEN_PSW} nexus.sk.com"
 			    echo "docker stack deploy (remote)"
-                sh "docker -H tcp://${DOCKER_REMOTE_HOST} stack deploy -c docker-stack.yaml --resolve-image changed --with-registry-auth fabric-net"
+                sh "docker -H tcp://${DOCKER_REMOTE_HOST} stack deploy -c docker-stack.yaml --resolve-image always --with-registry-auth fabric-net"
 			}
 		}
 	}
